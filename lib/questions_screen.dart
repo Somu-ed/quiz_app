2089a92929a44a782f30b97ee95a4bf448c55343
Soumyajeet Satpathy <jarvis@pop-os.localdomain>
2023-08-28 19:28:28 +0530

    formatting questions page

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +13,32 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return const Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'question...',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(
+            answerText: 'Answer text...',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer text...',
+            onTap: () {},
+          ),
+          AnswerButton(
+            answerText: 'Answer text...',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
